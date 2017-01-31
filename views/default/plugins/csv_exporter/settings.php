d19cce3f83c8ed8603d173dd47604ca6fa4849b6
Jerome Bakker <jeabakker@coldtrick.com>
2017-01-31 13:34:41 +0100

    chore: replaced deprecated function

--- views/default/plugins/csv_exporter/settings.php
+++ views/default/plugins/csv_exporter/settings.php
@@ -4,15 +4,17 @@ $plugin = elgg_extract('entity', $vars);
 
 $separator = csv_exporter_get_separator();
 
-echo elgg_view_input('text', [
-	'label' => elgg_echo('csv_exporter:settings:separator'),
+echo elgg_view_field([
+	'#type' => 'text',
+	'#label' => elgg_echo('csv_exporter:settings:separator'),
 	'name' => 'params[separator]',
 	'value' => $separator,
 	'maxlength' => 1,
 ]);
 
-echo elgg_view_input('text', [
-	'label' => elgg_echo('csv_exporter:settings:retention'),
+echo elgg_view_field([
+	'#type' => 'text',
+	'#label' => elgg_echo('csv_exporter:settings:retention'),
 	'help' => elgg_echo('csv_exporter:settings:retention:description'),
 	'name' => 'params[retention]',
 	'value' => $plugin->retention,
