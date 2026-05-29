4df66f360643670085f0ed78c4e72769a3c282ed
Jerôme Bakker <jeabakker@coldtrick.com>
2026-05-29 11:34:52 +0200

    chore: wrapping up v15.0

--- elgg-plugin.php
+++ elgg-plugin.php
@@ -5,7 +5,7 @@ require_once(dirname(__FILE__) . '/lib/functions.php');
 return [
 	'plugin' => [
 		'name' => 'CSV Exporter',
-		'version' => '14.0.1',
+		'version' => '15.0',
 	],
 	'entities' => [
 		[
